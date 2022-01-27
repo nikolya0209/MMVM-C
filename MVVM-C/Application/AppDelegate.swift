b8e966151b04addeb4821f176dc643dba51606f1
Nikolay Vinogradov <nikolya0209@mail.ru>
2022-01-27 22:07:26 +0300

    coordinator

--- MVVM-C/Application/AppDelegate.swift
+++ MVVM-C/Application/AppDelegate.swift
@@ -13,7 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var coordinator: AppCoordinator?
     
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let navController = UINavigationController()
         coordinator = AppCoordinator(navigationController: navController)
@@ -38,7 +37,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
